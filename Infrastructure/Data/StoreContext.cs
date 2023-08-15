03de71cd6b0a317f8bb325f6416b94b1e3d35170
OmarAhmed11 <67651788+OmarAhmed11@users.noreply.github.com>
2023-08-15 23:31:47 +0300

    Division of the project into 3 Class Libraries

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Core.Entities;
+
+namespace OnlineShop.Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ OnlineShop/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Core.Entities;
+using OnlineShop.Infrastructure.Data;
+
+namespace OnlineShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _context = storeContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> getProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> getProduct(int id)
+        {
+            var prod = await _context.Products.FindAsync(id);
+            return Ok(prod);
+        }
+    }
+}
